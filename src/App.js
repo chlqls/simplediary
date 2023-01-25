f70c5c5d6db6f5979cc17e0f414d52a77ed4709d
chlqls <chlqls212@gmail.com>
2023-01-25 09:43:42 +0900

    배열 사용하기 2 - 데이터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "최빈",
-    content: "하이 1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "최빈2",
-    content: "하이 2",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "최빈3",
-    content: "하이 3",
-    emotion: 5,
-    created_date: new Date().getTime(), //현재시간, 문자열로 다루기 쉽게 getTime
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "최빈",
+//     content: "하이 1",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "최빈2",
+//     content: "하이 2",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "최빈3",
+//     content: "하이 3",
+//     emotion: 5,
+//     created_date: new Date().getTime(), //현재시간, 문자열로 다루기 쉽게 getTime
+//   },
+// ];
 
 const App = () => {
+  const [data, setData] = useState([]); //state 끌어올리기(단방향 데이터 흐름, 역방향 이벤트 흐름)
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
