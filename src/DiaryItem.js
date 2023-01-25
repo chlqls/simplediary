87ffb379f43ccb947a28d0d6480fa6b4247ceb2f
chlqls <chlqls212@gmail.com>
2023-01-25 10:26:07 +0900

    배열 사용하기 4 - 데이터 수정하기

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,11 +1,45 @@
+import { useRef, useState } from "react";
+
 const DiaryItem = ({
-  onDelete,
+  onEdit,
+  onRemove,
   author,
   content,
   created_date,
   emotion,
   id,
 }) => {
+  const [isEdit, setIsEdit] = useState(false); //boolean값, true면 수정 중
+  const toggleIsEdit = () => setIsEdit(!isEdit); //boolean값 반전시킴
+
+  const [localContent, setLocalContent] = useState(content);
+
+  const localContentInput = useRef(); //수정할 때 focus사용
+
+  //삭제하기
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  //수정 취소
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -15,16 +49,30 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onDelete(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
