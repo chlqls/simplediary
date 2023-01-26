a8472ba9fec34f1a2f957bdbea230436f4bb1e09
chlqls <chlqls212@gmail.com>
2023-01-26 17:37:12 +0900

    최적화 1 - useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -14,7 +14,7 @@ const App = () => {
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
     ).then((res) => res.json());
-    console.log(res);
+    //console.log(res);
 
     const initData = res.slice(0, 20).map((it) => {
       //0~19까지, 총 20개만 추출
@@ -29,6 +29,10 @@ const App = () => {
 
     setData(initData);
   };
+
+  useEffect(() => {
+    getData(); //Mount 시점에 getData() 호출
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -57,14 +61,25 @@ const App = () => {
     );
   };
 
-  useEffect(() => {
-    getData(); //Mount 시점에 getData() 호출
-  }, []);
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작"); //처음 data 생성되고 한번, setData 해서 한번 더 실행됨
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]); //data.length가 변할 때만 콜백함수 실행
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis; //useMemo 사용하면 더이상 getDiaryAnalysis는 함수가 아니고 콜백함수가 반환하는 값임
 
   return (
     <div className="App">
       {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
