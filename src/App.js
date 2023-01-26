3500a0e52aea927a79979fb798906f85df269071
chlqls <chlqls212@gmail.com>
2023-01-26 15:11:48 +0900

    React에서 API 호출하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
+
+//https://jsonplaceholder.typicode.com/comments
 
 const App = () => {
   const [data, setData] = useState([]); //state 끌어올리기(단방향 데이터 흐름, 역방향 이벤트 흐름)
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0, 20).map((it) => {
+      //0~19까지, 총 20개만 추출
+      return {
+        author: it.email, //api의 email -> 작성자
+        content: it.body, //api의 body -> 본문
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -36,9 +57,13 @@ const App = () => {
     );
   };
 
+  useEffect(() => {
+    getData(); //Mount 시점에 getData() 호출
+  }, []);
+
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
